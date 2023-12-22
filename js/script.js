b4132a9310be8e20fa590369add9babd7fa3f0e7
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2023-12-22 15:20:01 +0545

    fully responsive set up

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
-$(document).ready(function(){
-    $(".one").owlCarousel({
-        items:3,
-        margin:20,
-    });
+$(document).ready(function () {
+  $(".one").owlCarousel({
+    margin: 20,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
   });
-  $(document).ready(function(){
-    $(".two").owlCarousel({
-        items:3,
-        margin:20,
-    });
+});
+$(document).ready(function () {
+  $(".two").owlCarousel({
+    margin: 20,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
   });
+});
